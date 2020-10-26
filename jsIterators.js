770edadb9387f81ed0d1aeae37e74ddd24df4f4e
Julia Ballo <ballo.julia@gmail.com>
2020-10-25 17:31:52 -0700

    First Commit

---
+++ jsIterators.js
@@ -0,0 +1,15 @@
+//exercise 33 
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for(let i = 0; i < people.length; i++) {
+    people[i].toUpperCase();
+}
+
+//exercise 34
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for(const nums of numbers) {
+    console.log(nums * nums);
+}
